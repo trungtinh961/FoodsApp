7bef7245a45503c73beb2633fdabf055a3ac9abe
Trung Tính Nguyễn <nguyentrungtinh961@gmail.com>
2020-09-27 17:30:47 +0700

    done lab 1

--- app/src/main/java/com/haerul/foodsapp/api/FoodApi.java
+++ app/src/main/java/com/haerul/foodsapp/api/FoodApi.java
@@ -14,18 +14,9 @@ import retrofit2.http.GET;
 
 public interface FoodApi {
 
-    @GET("latest.php")
+    @GET("search.php?f=a")
     Call<Meals> getMeal();
 
-    // TODO 12 also make the Call like getMeals() method for category
-
-    /*
-     * @GET (" url ") -->
-     *     this is the request annotation with REQUEST METHOD [GET]
-     *
-     * Call <Object> methodName(); -->
-     *     we will make the getCategoris () method with the Call <Category> || method meaning
-     *     that the result of the request [GET] will be accommodated into Object (Category)
-     *
-     */
+    @GET("categories.php")
+    Call<Categories> getCatogories();
 }
